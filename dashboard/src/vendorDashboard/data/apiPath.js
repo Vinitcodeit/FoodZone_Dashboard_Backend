7806d3b57a09ef6ce0f18048da856f8eeeb54692
Vinitcodeit <vtambralli412@gmail.com>
2024-08-01 20:21:38 +0530

    added different url

--- dashboard/src/vendorDashboard/data/apiPath.js
+++ dashboard/src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,4 @@
 // export const API_URL = "http://localhost:4000"
 
-export const API_URL = "https://foodzone-backend-nodejs.onrender.com"
+// export const API_URL = "https://foodzone-backend-nodejs.onrender.com"
+export const API_URL = "https://food-zone-backend-nodejs.vercel.app/"
